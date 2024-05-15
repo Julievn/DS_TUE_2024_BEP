6db49bc505f34935b391c38fc3b9510013670391
Julievn <julievn@gmail.com>
2024-05-15 22:41:36 +0200

    feat: add global Moran I value calculation

--- Src/main.py
+++ Src/main.py
@@ -9,6 +9,10 @@ import pandas as pd
 import libpysal as lps
 from libpysal.weights import Queen
 
+# Glocal Moran
+from esda.moran import Moran
+
+# Local Moran
 from esda.moran import Moran_Local
 from splot.esda import moran_scatterplot
 from splot.esda import lisa_cluster
@@ -194,6 +198,41 @@ def showCitiesInMap(cities_polygons_with_data, data_name, output_folder, year):
     save_plot_file_name = "choropleth_map_" + data_name + "_" + str(year) 
     plt.savefig(output_folder + '/' + save_plot_file_name)
 
+def calculateGlobalMoranI(cities_polygons_with_data, data_name, output_folder, year):
+     # Calculate weight matrix from the GeoDataFrame using Queen approach
+    queen_weight_matrix = Queen.from_dataframe(cities_polygons_with_data)
+
+    print(type(cities_polygons_with_data))
+    print(type(cities_polygons_with_data['GM_NAAM'].values))
+    print(cities_polygons_with_data['GM_NAAM'].values)
+
+    # <class 'pandas.core.series.Series'> when used inside this function.
+    # Outside this function is numpy array    
+    print(type(cities_polygons_with_data[data_name]))
+    print("Housing price values for year {}".format(year))
+    print(cities_polygons_with_data[data_name].values)
+
+    # house_prices = cities_polygons_with_house_prices['Average_House_Price'].to_numpy()
+    #print(type(house_prices))
+
+    moran_global = Moran(cities_polygons_with_data[data_name].values, queen_weight_matrix)
+    print("Glocal Moran spatial autocorrelation between cities")
+    print(moran_global)
+
+    # For all municipalities, there is only one average Global Moran’s I value, 
+    dataset_size = len(cities_polygons_with_data['GM_NAAM'].values) 
+    expect_moran_value = -1/(dataset_size - 1)
+    print("Expected value of Moran is {}!".format(expect_moran_value))
+    print("Glocal Moran I value = {}!".format(round(moran_global.I, 3)))
+
+    if moran_global.I > expect_moran_value:
+        print("Positve spatial correlations!")
+    else:
+        print("Negative spatial correlations!")
+
+    print("Moran EI value{}!".format(moran_global.EI))
+
+
 def calculateLocalMoranI(cities_polygons_with_data, data_name, output_folder, year):
      # Calculate weight matrix from the GeoDataFrame using Queen approach
     queen_weight_matrix = Queen.from_dataframe(cities_polygons_with_data)
@@ -211,12 +250,12 @@ def calculateLocalMoranI(cities_polygons_with_data, data_name, output_folder, ye
     #print(type(house_prices))
 
     moran_loc = Moran_Local(cities_polygons_with_data[data_name].values, queen_weight_matrix)
-    print("Moran spatial autocorrelation between cities")
+    print("Local Moran spatial autocorrelation between cities")
     print(moran_loc)
 
     # for each municipality, there is a relating Local Moran’s I value, 
     # as well as its own variance, z value, expected I, and variance of I
-    print("Moran EI value{}!".format(moran_loc.EI))
+    print("Local Moran EI value{}!".format(moran_loc.EI))
 
     fig, ax = moran_scatterplot(moran_loc)
     ax.set_xlabel('Cities')
@@ -276,6 +315,9 @@ def processHousePrices(path_to_house_prices_csv_file, path_to_shape_file):
         # Show cities in map. Only cities with housing prices will be shown.
         showCitiesInMap(cities_polygons_with_house_prices, data_name, output_housing_price_folder, year)
 
+        # Main part: calculate Global Moran I value
+        calculateGlobalMoranI(cities_polygons_with_house_prices, data_name, output_housing_price_folder, year)
+
         # Main part: calculate local Moran I value
         calculateLocalMoranI(cities_polygons_with_house_prices, data_name, output_housing_price_folder, year)
 
@@ -303,7 +345,10 @@ def processImmigration(path_to_immigration_csv_file, path_to_shape_file):
         # Show cities in map. Only cities with housing prices will be shown.
         showCitiesInMap(cities_polygons_with_immigration, data_name, output_immigration_folder, year)
 
-         # Main part: calculate Moran I value
+         # Main part: calculate Global Moran I value
+        calculateGlobalMoranI(cities_polygons_with_immigration, data_name, output_immigration_folder, year)
+
+         # Main part: calculate local Moran I value
         calculateLocalMoranI(cities_polygons_with_immigration, data_name, output_immigration_folder, year)
 
 def main():
