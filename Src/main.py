a1f5ad07972752d9a9b9d00389bf2d43fbf432f9
Julievn <julievn@gmail.com>
2024-05-28 20:56:03 +0200

    feat: extend list of municipal merging

--- Src/main.py
+++ Src/main.py
@@ -1,6 +1,7 @@
 ﻿# local libraries
 from house_prices import *
 from immigration import *
+from household_income import *
 from moran_calculation import *
 from utility import *
 
@@ -17,9 +18,9 @@ def main():
     municipality_name_code_mapping = readDictionaryFromCSVFile(
         path_to_name_code_mapping_file)
 
-    # Ignore old muncipalities that parts of them are merged into new ones as the new ones already contain the data
-    ignored_municipalities = ["Haaren", "Haarlemmerliede en Spaarnwoude",
-                              "Haren", "Leeuwarderadeel", "Littenseradiel", "Noordwijkerhout", "Rijnwaarden", "Weesp", "Millingen aan de Rijn", "Ubbergen"]
+    # Ignore old muncipalities that parts of them are merged into existing ones as the existing ones already contain the data
+    ignored_municipalities = ["Haaren", "Haarlemmerliede en Spaarnwoude", "Haren", "Leeuwarderadeel", "Littenseradiel",
+                              "Noordwijkerhout", "Rijnwaarden", "Weesp", "Millingen aan de Rijn", "Ubbergen", "Boarnsterhim", "Gaasterlân-Sleat", "Lemsterland", "Maasdonk", "De Friese Meren", "Skarsterlân", "Rijnwoude", "Schermer"]
 
     old_municipalities_lists = [
         ["Aalburg", "Werkendam", "Woudrichem"], [
@@ -38,22 +39,31 @@ def main():
         ["Giessenlanden", "Molenwaard"],
         ["Nuth", "Onderbanken", "Schinnen"],
         ["Schijndel", "Veghel", "Sint-Oedenrode"],
-        ["Groesbeek"]]
-    new_municipality_list = ["Altena", "Het Hogeland", "Purmerend", "Westerwolde", "Waadhoeke", "Hoeksche Waard", "Groningen", "Land van Cuijk",
-                             "Noardeast-Fryslân", "West Betuwe", "Westerkwartier", "Dijk en Waard", "Midden-Groningen", "Maashorst", "Vijfheerenlanden", "Eemsdelta", "Molenlanden", "Beekdaelen", "Meierijstad", "Berg en Dal"]
-    merged_year_list = [2019, 2019, 2022, 2018, 2018, 2019, 2019, 2022,
-                        2019, 2019, 2019, 2022, 2018, 2022, 2019, 2021, 2019, 2019, 2017, 2016]
-    merge_mode = 's'
+        ["Groesbeek"],
+        ["Spijkenisse", "Bernisse"],
+        ["Bergambacht", "Nederlek", "Vlist", "Ouderkerk", "Schoonhoven"],
+        ["Bussum", "Naarden", "Muiden"],
+        ["Boskoop"],
+        ["Graft-De Rijp"],
+        ["Zeevang"],
+        ["Brielle", "Hellevoetsluis", "Westvoorne"]]
+    merged_municipality_list = ["Altena", "Het Hogeland", "Purmerend", "Westerwolde", "Waadhoeke", "Hoeksche Waard", "Groningen", "Land van Cuijk", "Noardeast-Fryslân", "West Betuwe", "Westerkwartier", "Dijk en Waard", "Midden-Groningen",
+                                "Maashorst", "Vijfheerenlanden", "Eemsdelta", "Molenlanden", "Beekdaelen", "Meierijstad", "Berg en Dal", "Nissewaard", "Krimpenerwaard", "Gooise Meren", "Alphen aan den Rijn", "Alkmaar", "Edam-Volendam", "Voorne aan Zee"]
+    merged_year_list = [2019, 2019, 2022, 2018, 2018, 2019, 2019, 2022, 2019, 2019, 2019,
+                        2022, 2018, 2022, 2019, 2021, 2019, 2019, 2017, 2016, 2015, 2015, 2016, 2014, 2015, 2016, 2023]
 
     # Processing house prices
+    merge_mode = 'a'
     path_to_house_prices_csv_file = args[3]
     path_to_shape_file = args[5]
-    # processHousePrices(path_to_house_prices_csv_file, municipality_name_code_mapping, path_to_shape_file, ignored_municipalities)
+    processHousePrices(path_to_house_prices_csv_file, municipality_name_code_mapping, path_to_shape_file,
+                       ignored_municipalities, old_municipalities_lists, merged_municipality_list, merged_year_list, merge_mode)
 
     # Processing immigration
+    merge_mode = 's'
     path_to_immigration_csv_file = args[7]
     processImmigration(path_to_immigration_csv_file,
-                       municipality_name_code_mapping, path_to_shape_file, ignored_municipalities, old_municipalities_lists, new_municipality_list, merged_year_list, merge_mode)
+                       municipality_name_code_mapping, path_to_shape_file, ignored_municipalities, old_municipalities_lists, merged_municipality_list, merged_year_list, merge_mode)
 
 
 if __name__ == "__main__":
