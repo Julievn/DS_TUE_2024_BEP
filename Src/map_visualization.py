2906b03724e5b6010892b58f0eceb55ddaff71b7
Julievn <julievn@gmail.com>
2024-05-30 23:25:57 +0200

    feat: supporting exporting scatter plots for all years

--- Src/map_visualization.py
+++ Src/map_visualization.py
@@ -1,8 +1,11 @@
 ﻿import geopandas as gpd
+from matplotlib import colors
 import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from splot.esda import moran_scatterplot
 from utility import *
+import math
 
 important_cities_df = pd.DataFrame(
     {
@@ -16,6 +19,27 @@ important_cities_gdf = gpd.GeoDataFrame(
     # Amersfoort coordinate system
     important_cities_df, geometry=gpd.points_from_xy(important_cities_df.Longitude, important_cities_df.Latitude), crs="EPSG:28992"
 )
+
+
+def getMoranColors(local_moran_result):
+    # Convert all non-significant quadrants to zero
+    moran_local_quarants = local_moran_result.q
+    moran_local_quarants = np.where(
+        local_moran_result.p_sim >= 0.05, 0, moran_local_quarants)
+
+    #  1: "#d7191c", 2: "#89cff0", 3: "#2c7bb6", 4: "#fdae61", 0: "lightgrey"
+    #  With: 1 HH, 2 LH, 3 LL, 4 HL
+    quarants_colors_mapping = ["lightgrey",
+                               "#d7191c", "#89cff0", "#2c7bb6", "#fdae61"]
+
+    # For 2020 year there is no High Low values so the colors messed up.
+    # That's why we need to explicity create this color list and assign to each local moran value
+    moran_local_colors = []
+    for idx in range(len(moran_local_quarants)):
+        moran_local_color = quarants_colors_mapping[moran_local_quarants[idx]]
+        moran_local_colors.append(moran_local_color)
+
+    return moran_local_colors
 
 
 def showMunicipalitiesInMap(municipalities_polygons_with_data, data_name, output_folder, year, min_house_price, max_house_price):
@@ -66,9 +90,11 @@ def showMunicipalitiesInMap(municipalities_polygons_with_data, data_name, output
     # Save the current figure
     save_plot_file_name = "choropleth_map_" + data_name + "_" + str(year)
     plt.savefig(output_folder + '/' + save_plot_file_name)
+    plt.cla()
+    plt.close(fig)
 
 
-def exportChoroplethMapsAllYears(municipalities_polygons_with_house_prices_list, data_name, output_folder, min_house_price, max_house_price):
+def exportChoroplethMapsAllYears(municipalities_polygons_with_data_list, data_name, output_folder, min_house_price, max_house_price):
     # Polygon with data with color scale representing different values of data
     # number of rows and columns of the subplot grid
     # Choropleth maps (maps where the color of each shape is based on the value of an associated variable).
@@ -89,12 +115,12 @@ def exportChoroplethMapsAllYears(municipalities_polygons_with_house_prices_list,
     row = 0
     column = 0
     print("range is {}".format(
-        range(len(municipalities_polygons_with_house_prices_list))))
-    for idx in range(len(municipalities_polygons_with_house_prices_list)):
+        range(len(municipalities_polygons_with_data_list))))
+    for idx in range(len(municipalities_polygons_with_data_list)):
         row = int(idx / num_columns)
         column = idx % num_columns
 
-        municipalities_polygons_with_data = municipalities_polygons_with_house_prices_list[
+        municipalities_polygons_with_data = municipalities_polygons_with_data_list[
             idx]
         base = municipalities_polygons_with_data.plot(  # Generate a plot of a GeoDataFrame with matplotlib. If a column is specified, the plot coloring will be based on values in that column.
             # Values are used to color the plot. Ignored if color is also set.
@@ -127,3 +153,95 @@ def exportChoroplethMapsAllYears(municipalities_polygons_with_house_prices_list,
 
     save_plot_file_name = "choropleth_map_" + data_name + "_all_years"
     plt.savefig(output_folder + '/' + save_plot_file_name)
+    plt.cla()
+    plt.close(fig)
+
+
+def exportScatterPlotsAllYears(municipalities_polygons_with_data_list, data_name, local_moran_results_list, output_folder):
+    # Polygon with data with color scale representing different values of data
+    # number of rows and columns of the subplot grid
+    # Choropleth maps (maps where the color of each shape is based on the value of an associated variable).
+    num_columns = 3
+    num_rows = math.ceil(
+        len(municipalities_polygons_with_data_list) / num_columns)
+
+    # figsize: The size of the entire figure containing the subplots can be adjusted using the figure
+    # (figsize=(width, height)) function, where width and height are in inches.
+    # This indirectly changes the size of the subplots.
+    # This will create larger subplots, but they will still have the same proportions.
+    width_each_sub_plot = 5
+    fig, ax = plt.subplots(num_rows, num_columns, sharex=True, sharey=True, figsize=(width_each_sub_plot * num_columns, width_each_sub_plot * num_rows),
+                           gridspec_kw={
+                           'width_ratios': [width_each_sub_plot] * num_columns,
+                           'height_ratios': [width_each_sub_plot] * num_rows,
+                           'wspace': 0.05,
+                           'hspace': 0})
+
+    row = 0
+    column = 0
+    for year_idx in range(len(municipalities_polygons_with_data_list)):
+        row = int(year_idx / num_columns)
+        column = year_idx % num_columns
+
+        municipalities_polygons_with_data = municipalities_polygons_with_data_list[year_idx]
+        local_moran_result = local_moran_results_list[year_idx]
+
+        # Convert all non-significant quadrants to zero
+        moran_local_quarants = local_moran_result.q
+        moran_local_quarants = np.where(
+            local_moran_result.p_sim >= 0.05, 0, moran_local_quarants)
+
+        #  1: "#d7191c", 2: "#89cff0", 3: "#2c7bb6", 4: "#fdae61", 0: "lightgrey"
+        #  With: 1 HH, 2 LH, 3 LL, 4 HL
+        quarants_colors_mapping = ["lightgrey",
+                                   "#d7191c", "#89cff0", "#2c7bb6", "#fdae61"]
+
+        # For 2020 year there is no High Low values so the colors messed up.
+        # That's why we need to explicity create this color list and assign to each local moran value
+        moran_local_colors = []
+        for idx in range(len(moran_local_quarants)):
+            moran_local_color = quarants_colors_mapping[moran_local_quarants[idx]]
+            moran_local_colors.append(moran_local_color)
+
+        moran_local_colors = getMoranColors(local_moran_result)
+
+        print("Exporting scatter plots all years with local Moran quadrants size = {}, p_sim = {}, colors size = {}".format(
+            len(local_moran_result.q), len(local_moran_result.p_sim), len(moran_local_colors)))
+        print("Exporting scatter plots all years with local Moran quadrants = {}".format(
+            moran_local_quarants))
+        print("Exporting scatter plots all years with p_sim = {}".format(
+            local_moran_result.p_sim))
+        print("Exporting scatter plots all years with colors = {}".format(
+            moran_local_colors))
+
+        # ax: Matplotlib Axes instance, optional
+        # If given, the Moran plot will be created inside this axis. Default =None.
+        sub_fig, sub_ax = moran_scatterplot(
+            local_moran_result, True, p=0.05, ax=ax[row][column], scatter_kwds={'c': moran_local_colors})
+        if column == 0:
+            sub_ax.set_ylabel('Spatial Lag of ' + data_name)
+            sub_ax.set_xlabel("")
+        else:
+            # sub_ax.set_axis_off()
+            ax[row][column].set_axis_off()
+
+        title_sub_plot = str(getStartYear() + year_idx)
+        print("year_idx {}".format(year_idx))
+        print("getStartYear() {}".format(getStartYear()))
+        print("getStartYear() + iyear_idxdx {}".format(getStartYear() + year_idx))
+        print("title_sub_plot {}".format(title_sub_plot))
+        ax[row][column].set_title(
+            title_sub_plot, y=-0.05, fontsize=10)
+
+    for current_row in range(row, num_rows):
+        for next_removed_column in range(column + 1, num_columns):
+            print("Removing current_row {}, next_column {}".format(
+                current_row, next_removed_column))
+            fig.delaxes(ax[current_row][next_removed_column])
+
+    fig.suptitle(data_name + " for all years", fontsize=16)
+
+    save_plot_file_name = "local_moran_scatter_plots_" + data_name + "_all_years"
+    plt.savefig(output_folder + '/' + save_plot_file_name)
+    plt.cla()
+    plt.close(fig)
